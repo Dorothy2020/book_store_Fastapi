873666033faec55ded991a3fed373339dbd69025
Dorothy2020 <wangaarraakoth@gmail.com>
2023-09-12 10:01:57 +0300

    feat:add database connectivity

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Finding database Url
+SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+
+#create engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# create session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# declare mapping
+Base = declarative_base()
