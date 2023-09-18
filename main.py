20fba4c30d2c3c8dccf1a8deefa24d4438bbfbfa
Dorothy2020 <wangaarraakoth@gmail.com>
2023-09-18 07:14:51 +0300

    chore:made changes on mainfile

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from database import SessionLocal
+from sqlalchemy.orm import Session
+import database
+import models
+import schemas
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
 @app.get("/")
 def read_books():
     return{"message":"My very first book store api"}
+
+
+# Dependency to get the database session
+@app.post("/books/", response_model=schemas.Book)
+def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    db_book = models.Book(**book.dict())
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
